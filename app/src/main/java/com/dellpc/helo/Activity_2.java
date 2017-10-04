11d842dd109ec4b99c6a2dbbc117f2bb08b4a7b9
AmsterGet <amsterget@gmail.com>
2017-10-05 02:44:00 +0300

    3rd Lab

--- app/src/main/java/com/dellpc/helo/Activity_2.java
+++ app/src/main/java/com/dellpc/helo/Activity_2.java
@@ -8,6 +8,6 @@ public class Activity_2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_2);
+        setContentView(R.layout.login_layout);
     }
 }
--- app/src/main/java/com/dellpc/helo/MainActivity.java
+++ app/src/main/java/com/dellpc/helo/MainActivity.java
@@ -5,14 +5,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button but;
     private Button bt;
+    private Button third;
+    private Button inf;
     private TextView th;
+    private RelativeLayout layout;
+    private Toast toast;
     private int k;
+    int idx=0;
+    private static int colorvals[]={R.color.red,R.color.colorAccent,R.color.colorPrimary, R.color.black};
+    private static int colors[]={R.color.hmm, R.color.yellow,R.color.white, R.color.orange};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +34,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         but = (Button) findViewById(R.id.link);
         but.setOnClickListener(this);
+
+        third=(Button) findViewById(R.id.third);
+        third.setOnClickListener(this);
+        layout = (RelativeLayout) findViewById(R.id.layout);
+
+        inf = (Button) findViewById(R.id.inf);
+        inf.setOnClickListener(this);
     }
 
     @Override
@@ -36,10 +52,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             break;
             case R.id.button:
                 k++;
-                if (k%2!=0)th.setText("Hell or World!");
+                if (k%2!=0) th.setText("HEll OR WORLD!");
                 else th.setText("");
+                th.setTextColor(getResources().getColor(colorvals[idx]));
+                idx=(idx+1)%colorvals.length;
+            break;
+            case R.id.third:
+                layout.setBackgroundColor(getResources().getColor(colors[idx]));
+                idx=(idx+1)%colors.length;
+                toast = Toast.makeText(this, "Welcome to a new background color", Toast.LENGTH_SHORT);
+                toast.show();
+            break;
+            case R.id.inf:
+                toast = Toast.makeText(this, "Goncharik, Ivanovskaya, Khorovets are created this app", Toast.LENGTH_SHORT);
+                toast.show();
             break;
             default:break;
         }
     }
+
+
 }
